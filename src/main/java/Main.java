63da2a7ad05a4d718a0c1a34191d5fbeae1430b8
Anton Zapolsky <anton.zapolsky@softclub.by>
2016-11-17 10:51:09 +0300

    Added doc for clearConsole method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -109,6 +109,9 @@ public class Main {
         return result;
     }
 
+    /**
+     * Clears console from character
+     */
     public static void clearConsole() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
