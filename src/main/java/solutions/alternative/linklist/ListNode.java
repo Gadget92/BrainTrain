3f32192f00535a7f17121a92fc901ac5173deaf6
ZapAnton <wc3ft2@gmail.com>
2016-12-07 17:44:51 +0300

    Added nodeValue, previous and next node fields to ListNode class

--- src/main/java/solutions/alternative/linklist/ListNode.java
+++ src/main/java/solutions/alternative/linklist/ListNode.java
@@ -4,4 +4,14 @@ package solutions.alternative.linklist;
  * Created by zapolsky on 07.12.2016.
  */
 public class ListNode {
+    private int nodeValue;
+
+    private ListNode previousNode = null;
+
+    private ListNode nextNode = null;
+
+    ListNode(int nodeValue) {
+        this.nodeValue = nodeValue;
+    }
+
 }
