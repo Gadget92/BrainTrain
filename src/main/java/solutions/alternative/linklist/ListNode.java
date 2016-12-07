392c44b961b95c2557fadb979df994a205714a83
ZapAnton <wc3ft2@gmail.com>
2016-12-07 17:45:28 +0300

    Added getters and setters for ListNode fields

--- src/main/java/solutions/alternative/linklist/ListNode.java
+++ src/main/java/solutions/alternative/linklist/ListNode.java
@@ -14,4 +14,27 @@ public class ListNode {
         this.nodeValue = nodeValue;
     }
 
+    public int getNodeValue() {
+        return nodeValue;
+    }
+
+    public void setNodeValue(int nodeValue) {
+        this.nodeValue = nodeValue;
+    }
+
+    public ListNode getPreviousNode() {
+        return previousNode;
+    }
+
+    public void setPreviousNode(ListNode previousNode) {
+        this.previousNode = previousNode;
+    }
+
+    public ListNode getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(ListNode nextNode) {
+        this.nextNode = nextNode;
+    }
 }
