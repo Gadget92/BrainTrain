7d660a9f2ca454f3e4990dc680d19f0466f097b5
ZapAnton <wc3ft2@gmail.com>
2016-12-07 18:01:50 +0300

    Made simple test

--- src/main/java/solutions/alternative/Main.java
+++ src/main/java/solutions/alternative/Main.java
@@ -1,9 +1,16 @@
 package solutions.alternative;
 
+import solutions.alternative.linklist.MyLinkedList;
 
 public class Main {
 
     public static void main(String[] args) {
+        MyLinkedList linkedList = new MyLinkedList();
+
+        linkedList.addValue(10);
+        linkedList.addValue(5);
+
+        System.out.println(linkedList);
     }
 
 }
