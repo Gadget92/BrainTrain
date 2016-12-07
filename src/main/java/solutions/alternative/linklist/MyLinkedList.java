270093ba1c47be3282b692c0456dbfbac09767d3
ZapAnton <wc3ft2@gmail.com>
2016-12-07 17:56:54 +0300

    Implemented simple addValue method

--- src/main/java/solutions/alternative/linklist/MyLinkedList.java
+++ src/main/java/solutions/alternative/linklist/MyLinkedList.java
@@ -7,12 +7,25 @@ public class MyLinkedList {
 
     private ListNode tail = null;
 
+    public void addValue(int value) {
+        ListNode newNode = new ListNode(value);
+
+        if (tail == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            ListNode lastNode = tail;
+            lastNode.setNextNode(newNode);
+
+            tail = newNode;
+        }
+    }
+
     public ListNode getTail() {
         return tail;
     }
 
     public ListNode getHead() {
-
         return head;
     }
 }
