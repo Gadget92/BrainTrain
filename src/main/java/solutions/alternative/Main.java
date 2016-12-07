5ad72c4ffca59c1f86b86f1bfbc571f73be34183
ZapAnton <wc3ft2@gmail.com>
2016-12-07 21:14:32 +0200

    Made a test method for MyLinkedList

--- src/main/java/solutions/alternative/Main.java
+++ src/main/java/solutions/alternative/Main.java
@@ -5,12 +5,55 @@ import solutions.alternative.linklist.MyLinkedList;
 public class Main {
 
     public static void main(String[] args) {
+
+        myLinkedListTest();
+
+    }
+
+    private static void myLinkedListTest() {
         MyLinkedList linkedList = new MyLinkedList();
 
+        System.out.println("Created new list: " + linkedList);
+
         linkedList.addValue(10);
+
+        System.out.println("Added value 10 to list: " + linkedList);
+
         linkedList.addValue(5);
 
-        System.out.println(linkedList);
+        System.out.println("Added value 5 to list: " + linkedList);
+
+        linkedList.insertValue(0, 0);
+
+        linkedList.insertValue(100, linkedList.getSize());
+
+        linkedList.insertValue(999, 1);
+
+        System.out.println("Inserted value 0 to first position, value 100 to last position and 999 to position 1: " + linkedList);
+
+        System.out.println("Value at HEAD = " + linkedList.getHead().getNodeValue());
+
+        System.out.println("Value at TAIL = " + linkedList.getTail().getNodeValue());
+
+        System.out.println("Value at position 3 = " + linkedList.getValueAt(3));
+
+        System.out.println("Value at position 4 = " + linkedList.getValueAt(4));
+
+        linkedList.delete(0);
+
+        System.out.println("Deleted first element: " + linkedList);
+
+        linkedList.delete(linkedList.getSize() - 1);
+
+        System.out.println("Deleted last element: " + linkedList);
+
+        linkedList.delete(1);
+
+        System.out.println("Deleted second element: " + linkedList);
+
+        linkedList.clear();
+
+        System.out.println("Cleared list: " + linkedList);
     }
 
 }
