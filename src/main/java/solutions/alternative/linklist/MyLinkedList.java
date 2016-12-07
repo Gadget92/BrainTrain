8f6a1373d8bee58e9a2acf5a755d83490c3c8fe2
ZapAnton <wc3ft2@gmail.com>
2016-12-07 17:42:23 +0300

    Added tail and head variables and their getters

--- src/main/java/solutions/alternative/linklist/MyLinkedList.java
+++ src/main/java/solutions/alternative/linklist/MyLinkedList.java
@@ -2,4 +2,17 @@ package solutions.alternative.linklist;
 
 
 public class MyLinkedList {
+
+    private ListNode head = null;
+
+    private ListNode tail = null;
+
+    public ListNode getTail() {
+        return tail;
+    }
+
+    public ListNode getHead() {
+
+        return head;
+    }
 }
