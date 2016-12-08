8fdd00f3a9280a98d82610d0cb4618c95606195b
ZapAnton <wc3ft2@gmail.com>
2016-12-08 15:32:04 +0300

    Implemented findFileInDirectory method

--- src/main/java/solutions/alternative/find/FileFind.java
+++ src/main/java/solutions/alternative/find/FileFind.java
@@ -1,11 +1,13 @@
 package solutions.alternative.find;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class FileFind {
+    private List<String> foundFiles = new ArrayList<>();
 
     public void find(String sourceDirectoryName, String fileName) {
         File sourceDirectory = new File(sourceDirectoryName);
@@ -15,6 +17,17 @@ public class FileFind {
         }
 
         findFileInDirectory(sourceDirectory, fileName);
+
+        if (foundFiles.isEmpty()) {
+            System.out.println(
+                    String.format(
+                            "Could not find %s in directory %s",
+                            fileName,
+                            sourceDirectory));
+        } else {
+            System.out.println("Found files:");
+            foundFiles.forEach(System.out::println);
+        }
     }
 
     private void findFileInDirectory(File directory, String fileName) {
@@ -24,13 +37,17 @@ public class FileFind {
             return;
         }
 
-        List<File> subDirectories = Arrays.stream(directoryContent)
-                .filter(File::isDirectory)
-                .collect(Collectors.toList());
-
-        List<File> directoryFiles = Arrays.stream(directoryContent)
+        List<String> foundFilesList = Arrays.stream(directoryContent)
                 .filter(File::isFile)
-                .collect(Collectors.toList());
+                .filter(directoryFile -> directoryFile.getName().contains(fileName))
+                .map(File::getPath)
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        foundFiles.addAll(foundFilesList);
+
+        Arrays.stream(directoryContent)
+                .filter(File::isDirectory)
+                .forEach(subDirectory -> findFileInDirectory(subDirectory, fileName));
 
     }
 
