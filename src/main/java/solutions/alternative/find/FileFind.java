1d9e07a67e3f02ff019bb3a0a95ea19d898f9a9b
ZapAnton <wc3ft2@gmail.com>
2016-12-08 14:45:38 +0300

    Added findFileInDirectory method

--- src/main/java/solutions/alternative/find/FileFind.java
+++ src/main/java/solutions/alternative/find/FileFind.java
@@ -1,16 +1,40 @@
 package solutions.alternative.find;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class FileFind {
 
-    public void find(String sourceDirectoryName, String filename) {
+    public void find(String sourceDirectoryName, String fileName) {
         File sourceDirectory = new File(sourceDirectoryName);
 
         if (!validDirectoryName(sourceDirectory)) {
             return;
         }
 
+        findFileInDirectory(sourceDirectory, fileName);
+    }
+
+    private void findFileInDirectory(File directory, String fileName) {
+        File[] directoryContent = directory.listFiles();
+
+        if (directoryContent == null) {
+            return;
+        }
+
+        List<File> subDirectories = Arrays.stream(directoryContent)
+                .filter(File::isDirectory)
+                .collect(Collectors.toList());
+
+        List<File> directoryFiles = Arrays.stream(directoryContent)
+                .filter(File::isFile)
+                .collect(Collectors.toList());
+
+        subDirectories.forEach(subDirectory -> System.out.print(subDirectory + " "));
+
+        directoryFiles.forEach(directoryFile -> System.out.print(directoryFile + " "));
     }
 
     private boolean validDirectoryName(File sourceDirectory) {
