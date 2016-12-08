2a9ebe8d5dbb1a0b090c28342c106be321ff8b75
ZapAnton <wc3ft2@gmail.com>
2016-12-08 14:37:05 +0300

    Added method for checking if sourceDirectoryName is valid

--- src/main/java/solutions/alternative/find/FileFind.java
+++ src/main/java/solutions/alternative/find/FileFind.java
@@ -1,9 +1,38 @@
 package solutions.alternative.find;
 
+import java.io.File;
+
 public class FileFind {
 
-    public static void find(String source, String filename) {
+    public void find(String sourceDirectoryName, String filename) {
+        File sourceDirectory = new File(sourceDirectoryName);
 
+        if (!validDirectoryName(sourceDirectory)) {
+            return;
+        }
+
+    }
+
+    private boolean validDirectoryName(File sourceDirectory) {
+        if (!sourceDirectory.exists()) {
+            System.out.println(String.format(
+                    "Error! Directory %s does not exists",
+                    sourceDirectory.getName())
+            );
+
+            return false;
+        }
+
+        if (!sourceDirectory.isDirectory()) {
+            System.out.println(String.format(
+                    "Error! Object at %s is not a directory",
+                    sourceDirectory.getPath())
+            );
+
+            return false;
+        }
+
+        return true;
     }
 
 }
